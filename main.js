731bc9e5b658a211dc5e9ef4467b7eb5d45b7ae3
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-10 23:16:18 -0800

    i have added some line in html and js

---
+++ main.js
@@ -0,0 +1,7 @@
+// variabless in javascript
+
+var name = 'My name is joney'
+var age = 21
+document.querySelector('#showName').innerHTML = name
+document.querySelector('#showAge').innerHTML = age
+
